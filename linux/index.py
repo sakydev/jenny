3a4427303fd7534cf3874e37466dc410b5467980
Saqib Razzaq <saqibrzzaq@gmail.com>
2021-08-18 17:46:51 +0500

    Run commands directly

--- linux/index.py
+++ linux/index.py
@@ -1,7 +1,16 @@
 import modules.jenny as jenny
+import sys
+
 
 greetings = True
+singleRan = False
+
 while True:
-	command = jenny.ask(greetings=greetings)
+	if len(sys.argv) > 1 and not singleRan:
+		command = ' '.join(sys.argv)
+		singleRan = True
+	else:
+		command = jenny.ask(greetings=greetings)
+
 	jenny.process(command)
 	greetings = False
--- linux/jenny.sh
+++ linux/jenny.sh
@@ -1,1 +1,5 @@
-python %~dp0/index.py
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+python $SCRIPT_DIR/index.py $1 $2 $3
